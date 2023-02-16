74eea760bdfc7b89b0adbdaf1b1fbf498ab8a62b
auronalidemaj <auronalidemaj@gmail.com>
2023-02-16 15:37:59 +0100

    added contact to nav and footer

---
+++ javascript/contact.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('#contact-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#name').value;
+  const email = document.querySelector('#email').value;
+  const message = document.querySelector('#message').value;
+
+  if (!name || !email || !message) {
+    alert('Please fill out all the fields');
+    return;
+  }
+
+  alert(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
+});
