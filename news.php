9e06765763ac4098df531fcd5e3cdc82115cd539
LeotrimLajqi <leotrimlajqi18@gmail.com>
2023-02-17 19:28:57 +0100

    update

---
+++ news.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News</title>
+    <link rel="stylesheet" href="css/news.css">
+    <link rel="stylesheet" href="css/headerandfooter.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+</head>
+<body>
+
+    <div class = "slider">
+        <img src="img/news/image-asset.jpeg" >
+        <img src="img/news/image-asset.jpeg" >
+
+    </div>
+    <div class = "b"></div>
+    <div class = "c"></div>
+    <div class = "d"></div>
+    <div class = "e"></div>
+</body>
+</html>
